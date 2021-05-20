54e76724a150abd730fe421bf09381a107e02229
wangsongwen <2544894086wsw@gmail.com>
2021-05-20 16:17:53 +0800

    服务端---Http Post请求接口

--- src/main/java/com/wsw/multihttprequest/controller/MultiHttpRequestController.java
+++ src/main/java/com/wsw/multihttprequest/controller/MultiHttpRequestController.java
@@ -1,10 +1,19 @@
 package com.wsw.multihttprequest.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @Author WangSongWen
  * @Date: Created in 16:04 2021/5/20
  * @Description: 服务端---Http Post请求接口
  */
+@RestController
+@RequestMapping("/wsw")
 public class MultiHttpRequestController {
-
+    @PostMapping("/hello")
+    public String sayHello(){
+        return "hello";
+    }
 }
